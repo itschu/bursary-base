a3d3694af236bce05f86e809484c6d4af7c500b9
itschu <palmerbideconcepts@gmail.com>
2022-11-19 12:47:39 +0100

    added tailwincss to project

---
+++ app/head.tsx
@@ -0,0 +1,14 @@
+import "../styles/globals.css";
+
+export default function Head() {
+	return (
+		<>
+			<title></title>
+			<meta
+				content="width=device-width, initial-scale=1"
+				name="viewport"
+			/>
+			<link rel="icon" href="/favicon.ico" />
+		</>
+	);
+}
---
+++ app/page.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+const Page = () => {
+	return (
+		<div className="bg-red-400">
+			<p className="text-4xl text-red-400">paragraph</p>
+			Pages
+		</div>
+	);
+};
+
+export default Page;
